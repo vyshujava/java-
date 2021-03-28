b8946ec99c6cece42e18a1caa7d2e6a81aabf2fd
vyshujava <56514365+vyshujava@users.noreply.github.com>
2021-03-29 03:27:24 +0530

    Add files via upload

---
+++ KgtoLb.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class KgtoLb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter the weight ");
+		
+		
+		int kg = sc.nextInt();
+		
+		System.out.println("your weight is: " +kg + " kg" );
+		
+		// kg into pound 
+		
+		double pound = (int) (kg * 2.205);
+		
+		System.out.println(pound);
+		
+		
+		System.out.println("your weight in pound is : "+pound + " lb " );
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
